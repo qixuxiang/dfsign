08f567b60ca79f736d1d4215047910ff4f6cd7aa
ixhorse <cyfhorse@gmail.com>
2019-06-01 15:05:49 +0800

    update

--- pytorch-deeplab-xception/test.sh
+++ pytorch-deeplab-xception/test.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python test.py --backbone resnet --workers 4 --test-batch-size 4 --gpu-ids 0 --dataset tt100k --weight "run/dfsign/resnet-region/experiment_0/model_best.pth.tar"
+CUDA_VISIBLE_DEVICES=0 python test.py --backbone resnet --workers 4 --test-batch-size 1 --gpu-ids 0 --dataset tt100k --weight "run/dfsign/resnet-region/experiment_4/checkpoint_2.pth.tar"
